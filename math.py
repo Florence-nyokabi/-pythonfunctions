2449b570d44bc3825f12631a287031f4fb404d82
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-03-31 20:44:29 +0300

    Functions

---
+++ math.py
@@ -0,0 +1,19 @@
+def add (a, b):
+    answer = a + b 
+    return answer
+
+def subtract(c, d):
+    answer1 = c - d
+    return answer1
+
+def multiply(e, f):
+    answer2 = e * f
+    return answer2
+
+def divide(g, h):
+    answer3 = g / h
+    return answer3
+
+def remainder(i, j):
+    answer4 = i % j
+    return answer4
